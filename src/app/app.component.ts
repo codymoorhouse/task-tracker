ae77b376986320348c3532ef023e8a3c414378b9
Cody Moorhouse <cody.moorhouse@pderas.com>
2019-03-19 20:27:26 -0600

    Import tasks into main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'task-tracker';
+  title = 'Task Tracker';
+  tasks: Task[];
+
+  ngOnInit() {
+    this.setTasks();
+  }
+
+  setTasks() {
+    this.tasks = Task.tasks.map(task => task.clone());
+  }
 }
