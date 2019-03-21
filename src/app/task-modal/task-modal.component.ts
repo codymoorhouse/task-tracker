4f70c8575f39e2f01bcf471c053e789d7806f4f6
Cody Moorhouse <cody.moorhouse@pderas.com>
2019-03-21 00:06:00 -0600

    Allow tasks to be added

--- src/app/task-modal/task-modal.component.ts
+++ src/app/task-modal/task-modal.component.ts
@@ -10,16 +10,44 @@ export class TaskModalComponent implements OnInit {
   @Output() added = new EventEmitter<Task>();
   @Output() closed = new EventEmitter<boolean>();
 
-  public task: Task;
+  protected name: string;
+  protected description: string;
+  protected estimate: number;
 
+  protected errors: {
+    name: string,
+    description: string,
+    estimate: string
+  } = {name: null, description: null, estimate: null}
   constructor() { }
 
-  ngOnInit() {
-    this.task = new Task(4, 'echk', 'cec', 2);
+  ngOnInit(): void {
+    this.name = null;
+    this.description = null;
+    this.estimate = null;
   }
 
   add() {
-    this.added.emit(this.task);
+    if (this.isValidTask()) {
+      this.added.emit(new Task(4, this.name, this.description, this.estimate));
+      this.closed.emit(true);
+    } else {
+      this.setErrors();
+    }
+  }
+
+  isValidTask() {
+    return this.name && this.description && this.estimate;
+  }
+
+  hasErrors() {
+    return this.errors.name || this.errors.description && this.errors.estimate;
+  }
+
+  setErrors() {
+    this.errors.name = !this.name ? 'A name must be specified' : null;
+    this.errors.description = !this.description ? 'The description must be specified' : null;
+    this.errors.estimate = !this.estimate ? 'A valid estimate must be specified.' : null;
   }
 
   close(event) {
